bde2776df44113467f4905a55157d5f181d53053
PioneerFromUSSR <kosshilov@gmail.com>
2021-04-05 08:32:13 +0300

    Change repo name

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t kernelpanic33/web:latest -t
-docker build -t kernelpanic33/openvidu-server:latest -t
-docker build -t kernelpanic33/openvidu-server-kms:latest -t
+docker build -t pioneerfromussr/web:latest -t
+docker build -t pioneerfromussr/openvidu-server:latest -t
+docker build -t pioneerfromussr/openvidu-server-kms:latest -t
 
-docker push kernelpanic33/web:latest
-docker push kernelpanic33/openvidu-server:latest
-docker push kernelpanic33/openvidu-server-kms:latest
+docker push pioneerfromussr/web:latest
+docker push pioneerfromussr/openvidu-server:latest
+docker push pioneerfromussr/openvidu-server-kms:latest
 
 kubectl apply -f k8s
 kubectl set image deployments/web-deployment
-    web=kernelpanic33/web:latest
+    web=pioneerfromussr/web:latest
 kubectl set image deployments/openvidu-server-deployment
-    openvidu-server=kernelpanic33/openvidu-server:latest
+    openvidu-server=pioneerfromussr/openvidu-server:latest
 kubectl set image deployments/openvidu-server-kms-deployment
-    openvidu-server-kms=kernelpanic33/openvidu-server-kms:latest
+    openvidu-server-kms=pioneerfromussr/openvidu-server-kms:latest
